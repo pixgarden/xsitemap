64adfc55e7363b0a63837711268b46f3ef0939f8
François JOLY <francois@gokam.fr>
2019-02-11 10:27:50 +0000

    dealing with missing / in domain name

--- R/guessXMLSitemap.R
+++ R/guessXMLSitemap.R
@@ -22,6 +22,12 @@ guessXMLSitemap <- function(urltocheck) {
     )
 
 
+  if("/" != substr(urltocheck, nchar(urltocheck),  nchar(urltocheck))){
+
+    urltocheck <- paste0(urltocheck, "/")
+
+  }
+
     user_agent <-
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
 
