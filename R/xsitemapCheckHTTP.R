19b68c79db4b74e49f6e64bf38b67aabec3bcd40
François JOLY <francois@gokam.fr>
2019-02-18 19:19:39 +0000

    are you sure ? prompte for more than 1K urls

--- R/xsitemapCheckHTTP.R
+++ R/xsitemapCheckHTTP.R
@@ -9,7 +9,17 @@
 #'
 xsitemapCheckHTTP <- function(sitemap) {
   message(paste("xsitemapCheckHTTP :", nrow(sitemap), " URL(s) to check"))
-
+  if (nrow(sitemap) < 1000) {
+    if (!askYesNo(
+      paste("Are you sure you want to crawl", nrow(sitemap), "URLs"),
+      default = TRUE,
+      prompts = getOption("askYesNo", gettext(c(
+        "Yes", "No", "Cancel"
+      )))
+    )) {
+      return(NULL)
+    }
+  }
   user_agent <-
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
 
